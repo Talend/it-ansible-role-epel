c3c48e37debd3857b9381a172ca136bea1f1df8a
Mehdi MAHFOUDI <mmahfoudi@talend.com>
2019-05-04 20:07:05 +0200

    Add molecule tests
    Signed-off-by: Mehdi MAHFOUDI <mmahfoudi@talend.com>

---
+++ molecule/default/tests/test_epel.py
@@ -0,0 +1,27 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_epel_package(host):
+    p = host.package('epel-release')
+
+    assert p.is_installed
+
+
+def test_epel_file(host):
+    f = host.file('/etc/yum.repos.d/epel.repo')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o644
+
+
+def test_epel_enabled(host):
+    cmd = host.run('yum repolist enabled')
+
+    assert 'epel' in cmd.stdout
